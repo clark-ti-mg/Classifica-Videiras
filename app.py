154bd77fac2bc5098f95e991d5fc3399b8b61f27
clark-ti-mg <153373604+clark-ti-mg@users.noreply.github.com>
2025-07-14 11:39:36 +0000

    app.py criado

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 import streamlit as st
+
+
+def main():
+    st.set_page_config(
+        page_title="Classifica folhas de Videiras"
+    )
+    st.write("# Classifica folhas de videiras")
+    # carregar o modelo
+
+    # carregar a imagem
+
+    # classificar a imagem
+
+    if __name__ == "__main__":
+        main()
